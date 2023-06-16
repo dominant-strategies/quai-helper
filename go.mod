bda04a2c971202b88251f91b3ff23b640468ff7a
gop <gameofpointers@proton.me>
2023-06-16 10:41:20 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,43 @@
+module github.com/dominant-strategies/quai-helper
+
+go 1.20
+
+require (
+	github.com/dominant-strategies/go-quai v0.6.0-pre.4.0.20230616152455-e02eab483b76
+	github.com/spf13/viper v1.16.0
+)
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/deckarep/golang-set v0.0.0-20180603214616-504e848d77ea // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/ledgerwatch/secp256k1 v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/quaiclient"
+	"github.com/spf13/viper"
+)
+
+type NodeClient struct {
+	Client    *quaiclient.Client
+	Connected bool
+}
+
+// Clients for RPC connection to the Prime, region, & zone ports belonging to the
+// slice we are actively mining
+type SliceClients struct {
+	PrimeClient   NodeClient
+	RegionClients []NodeClient
+	ZoneClients   [][]NodeClient
+}
+
+func NewSliceClients() SliceClients {
+	var sliceClients SliceClients
+	sliceClients.RegionClients = make([]NodeClient, common.HierarchyDepth)
+	sliceClients.ZoneClients = make([][]NodeClient, common.HierarchyDepth)
+	for i := 0; i < common.HierarchyDepth; i++ {
+		sliceClients.ZoneClients[i] = make([]NodeClient, common.HierarchyDepth)
+	}
+	return sliceClients
+}
+
+func (sc *SliceClients) allNodesConnected() bool {
+	return sc.PrimeClient.Connected &&
+		sc.RegionClients[0].Connected && sc.RegionClients[1].Connected && sc.RegionClients[2].Connected &&
+		sc.ZoneClients[0][0].Connected && sc.ZoneClients[0][1].Connected && sc.ZoneClients[0][2].Connected &&
+		sc.ZoneClients[1][0].Connected && sc.ZoneClients[1][1].Connected && sc.ZoneClients[1][2].Connected &&
+		sc.ZoneClients[2][0].Connected && sc.ZoneClients[2][1].Connected && sc.ZoneClients[2][2].Connected
+}
+
+var (
+	GenesisHash = common.HexToHash("0x4c35b1216decc6aa2431fa2d2a1c68f15d70f83a309041ed1bfef5ad6592a3d4")
+)
+
+func printBadHashes(primeHash common.Hash, regionHashes []common.Hash, zoneHashes [][]common.Hash) {
+	fmt.Printf("HeirarchyBadHashes{\n")
+	fmt.Printf("\tPrimeContext: common.HexToHash(\"%s\"),\n", primeHash)
+	fmt.Printf("\tRegionContext: []common.Hash{\n")
+	for _, regionHash := range regionHashes {
+		fmt.Printf("\t\tcommon.HexToHash(\"%s\"),\n", regionHash)
+	}
+	fmt.Printf("\t},\n")
+	fmt.Printf("\tZoneContext: [][]common.Hash{\n")
+	for _, zoneHashSlice := range zoneHashes {
+		fmt.Printf("\t\t[]common.Hash{\n")
+		for _, zoneHash := range zoneHashSlice {
+			fmt.Printf("\t\t\tcommon.HexToHash(\"%s\"),\n", zoneHash)
+		}
+		fmt.Printf("\t\t},\n")
+	}
+	fmt.Printf("\t},\n")
+	fmt.Printf("}\n")
+}
+
+func main() {
+	config, err := LoadConfig("")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	sliceClients := connectToSlice(config)
+
+	primeBadHash := common.HexToHash("0x00000d94c1dc7347a7c1f5c25cbe684104ec9da4da0898805fa4ce7e449fa80a")
+	primeBadHeader := sliceClients.PrimeClient.Client.HeaderByHash(context.Background(), primeBadHash)
+	primeTermini := GenerateTermini(sliceClients, 0, 0, primeBadHeader.ParentHash())
+	regionTermini := make([][]common.Hash, common.HierarchyDepth)
+	for i := 0; i < common.HierarchyDepth; i++ {
+		regionTermini[i] = GenerateTermini(sliceClients, 1, i, primeTermini[i])
+	}
+
+	regionBadHashes := make([]common.Hash, common.HierarchyDepth)
+	for i := 0; i < common.HierarchyDepth; i++ {
+		regionTerminiHeader := sliceClients.RegionClients[i].Client.HeaderByHash(context.Background(), primeTermini[i])
+		regionBadHashes[i] = sliceClients.RegionClients[i].Client.HeaderByNumber(context.Background(), "0x"+strings.ToUpper(fmt.Sprintf("%x", regionTerminiHeader.NumberU64(common.REGION_CTX)+1))).Hash()
+	}
+
+	zoneBadHashes := make([][]common.Hash, common.HierarchyDepth)
+	for i := 0; i < common.HierarchyDepth; i++ {
+		zoneBadHashes[i] = make([]common.Hash, common.HierarchyDepth)
+	}
+	for i := 0; i < common.HierarchyDepth; i++ {
+		for j := 0; j < common.HierarchyDepth; j++ {
+			zoneTerminiHeader := sliceClients.ZoneClients[i][j].Client.HeaderByHash(context.Background(), regionTermini[i][j])
+			zoneBadHashes[i][j] = sliceClients.ZoneClients[i][j].Client.HeaderByNumber(context.Background(), "0x"+strings.ToUpper(fmt.Sprintf("%x", zoneTerminiHeader.NumberU64(common.ZONE_CTX)+1))).Hash()
+		}
+	}
+	// go forward and collect the next block
+	printBadHashes(primeBadHash, regionBadHashes, zoneBadHashes)
+
+	return
+}
+
+func GenerateTermini(sc SliceClients, ctx, index int, hash common.Hash) []common.Hash {
+	termini := make([]common.Hash, 4)
+
+	var client *quaiclient.Client
+	if ctx == common.PRIME_CTX {
+		client = sc.PrimeClient.Client
+	} else if ctx == common.REGION_CTX {
+		client = sc.RegionClients[index].Client
+	}
+
+	isTerminiFull := func(termini []common.Hash) bool {
+		var count int
+		for _, t := range termini {
+			if (t != common.Hash{}) {
+				count++
+			}
+		}
+		return count == 4
+	}
+
+	// This header hash serves as the Terminus value
+	termini[3] = hash
+	if ctx == common.PRIME_CTX || ctx == common.REGION_CTX {
+		header := client.HeaderByHash(context.Background(), hash)
+		termini[header.Location()[ctx]] = hash
+		parent := header
+		for {
+			if isTerminiFull(termini) {
+				break
+			}
+			if parent.Hash() == GenesisHash {
+				for i, t := range termini {
+					if (t == common.Hash{}) {
+						termini[i] = GenesisHash
+					}
+				}
+				break
+			}
+			header := client.HeaderByHash(context.Background(), parent.ParentHash(ctx))
+			if (termini[header.Location()[ctx]] == common.Hash{}) {
+				termini[header.Location()[ctx]] = header.Hash()
+			}
+			parent = header
+		}
+	}
+	return termini
+}
+
+// connectToSlice takes in a config and retrieves the Prime, Region, and Zone client
+// that is used for mining in a slice.
+func connectToSlice(config Config) SliceClients {
+	var err error
+	clients := NewSliceClients()
+
+	for {
+		if clients.allNodesConnected() {
+			break
+		}
+		clients.PrimeClient.Client, err = quaiclient.Dial(config.PrimeURL)
+		if err != nil {
+			log.Println("Unable to connect to node:", "Prime", config.PrimeURL)
+		} else {
+			clients.PrimeClient.Connected = true
+		}
+		for i := 0; i < common.HierarchyDepth; i++ {
+			clients.RegionClients[i].Client, err = quaiclient.Dial(config.RegionURLs[i])
+			if err != nil {
+				log.Println("Unable to connect to node:", "Region", config.RegionURLs[i])
+			} else {
+				clients.RegionClients[i].Connected = true
+			}
+			for j := 0; j < common.HierarchyDepth; j++ {
+				clients.ZoneClients[i][j].Client, err = quaiclient.Dial(config.ZoneURLs[i][j])
+				if err != nil {
+					log.Println("Unable to connect to node:", "Zone", config.ZoneURLs[i][j])
+				} else {
+					clients.ZoneClients[i][j].Connected = true
+				}
+			}
+		}
+	}
+	return clients
+}
+
+// Config holds the configuration parameters for quai-manager
+type Config struct {
+	PrimeURL   string
+	RegionURLs []string
+	ZoneURLs   [][]string
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err = viper.ReadInConfig()    // Find and read the config file
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
